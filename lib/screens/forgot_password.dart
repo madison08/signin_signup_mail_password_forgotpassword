795a0624353b1ef7f8d58a5ce6b771b49b8485c2
madison08 <madisonamoa@gmail.com>
2022-05-05 08:15:27 +0000

    forgot password screen

--- lib/screens/forgot_password.dart
+++ lib/screens/forgot_password.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class ForgotPasswordScreen extends StatefulWidget {
@@ -8,8 +10,100 @@ class ForgotPasswordScreen extends StatefulWidget {
 }
 
 class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
+  final _formKey = GlobalKey<FormState>();
+
+  var email = "";
+
+  final emailController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Renitialiser mot de passe',
+        ),
+        actions: [],
+      ),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(20.0),
+            child: FlutterLogo(
+              size: 150.0,
+            ),
+          ),
+          Container(
+            child: Text(
+              'Un lien de renitialisation seras envoyer sur votre email',
+              style: TextStyle(
+                fontSize: 20.0,
+              ),
+            ),
+          ),
+          Expanded(
+              child: Form(
+            key: _formKey,
+            child: Padding(
+              padding: EdgeInsets.symmetric(
+                vertical: 10.0,
+                horizontal: 15.0,
+              ),
+              child: ListView(
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 10.0),
+                    child: TextFormField(
+                      autofocus: false,
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        border: OutlineInputBorder(),
+                      ),
+                      controller: emailController,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Veuillez entrer votre mail';
+                        } else if (!value.contains('@')) {
+                          return 'Entrer une adresse mail correcte';
+                        }
+
+                        return null;
+                      },
+                    ),
+                  ),
+                  Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        ElevatedButton(
+                          onPressed: () {},
+                          child: Text('Envoyer mail'),
+                        ),
+                        SizedBox(
+                          width: 10.0,
+                        ),
+                        ElevatedButton(
+                          onPressed: () {},
+                          child: Text('Se connecter'),
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                    child: Row(
+                      children: <Widget>[
+                        Text('Vous n\'avez pas de compte ?'),
+                        TextButton(
+                            onPressed: () {}, child: Text('Inscrivez-vous'))
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )),
+        ],
+      ),
+    );
   }
 }
