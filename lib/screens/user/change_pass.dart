51610d7414d24e2dce79abacbdad3e8bc14f22bd
madison08 <madisonamoa@gmail.com>
2022-05-07 02:21:47 +0000

    main screen

---
+++ lib/screens/user/change_pass.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ChangePassScreen extends StatefulWidget {
+  const ChangePassScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ChangePassScreen> createState() => _ChangePassScreenState();
+}
+
+class _ChangePassScreenState extends State<ChangePassScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/screens/user_main_screen.dart
+++ lib/screens/user_main_screen.dart
@@ -1,4 +1,9 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:signin_mail_password_forgotpassword/screens/user/change_pass.dart';
+import 'package:signin_mail_password_forgotpassword/screens/user/dashboard.dart';
+import 'package:signin_mail_password_forgotpassword/screens/user/profile.dart';
 
 class UserMainScreen extends StatefulWidget {
   const UserMainScreen({Key? key}) : super(key: key);
@@ -8,8 +13,43 @@ class UserMainScreen extends StatefulWidget {
 }
 
 class _UserMainScreenState extends State<UserMainScreen> {
+  int _selectedIndex = 0;
+
+  static List<Widget> _widgetOptions = <Widget>[
+    DashboardScreen(),
+    ProfileScreen(),
+    ChangePassScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: _widgetOptions.elementAt(_selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Dashboard',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Change',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue,
+        onTap: _onItemTapped,
+      ),
+    );
   }
 }
