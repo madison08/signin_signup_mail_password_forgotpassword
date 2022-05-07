d7bb36eb0b38d6d5b9060e9447f4fa28a77a4f31
madison08 <madisonamoa@gmail.com>
2022-05-07 01:02:35 +0000

    reset password

--- lib/screens/forgot_password.dart
+++ lib/screens/forgot_password.dart
@@ -1,6 +1,9 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:signin_mail_password_forgotpassword/screens/login_screen.dart';
+import 'package:signin_mail_password_forgotpassword/screens/signup_screen.dart';
 
 class ForgotPasswordScreen extends StatefulWidget {
   const ForgotPasswordScreen({Key? key}) : super(key: key);
@@ -15,6 +18,49 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
   var email = "";
 
   final emailController = TextEditingController();
+
+  resetPassword() async {
+    try {
+      await FirebaseAuth.instance.sendPasswordResetEmail(email: email.trim());
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(
+            'Le mot de de renitialisation a ete envoyer',
+          ),
+        ),
+      );
+
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) {
+            return LoginScreen();
+          },
+        ),
+      );
+    } on FirebaseAuthException catch (err) {
+      if (err.code == 'user-not-found') {
+        print('utilisateur non trouver');
+
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(
+              'l\'utilisateur n\'existe pas',
+            ),
+          ),
+        );
+      }
+    }
+  }
+
+  @override
+  void dispose() {
+    emailController.dispose();
+
+    // TODO: implement dispose
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,6 +80,9 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
             ),
           ),
           Container(
+            margin: EdgeInsets.symmetric(
+              horizontal: 15.0,
+            ),
             child: Text(
               'Un lien de renitialisation seras envoyer sur votre email',
               style: TextStyle(
@@ -76,14 +125,30 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: <Widget>[
                         ElevatedButton(
-                          onPressed: () {},
+                          onPressed: () {
+                            if (_formKey.currentState!.validate()) {
+                              setState(() {
+                                email = emailController.text;
+                              });
+                              resetPassword();
+                            }
+                          },
                           child: Text('Envoyer mail'),
                         ),
                         SizedBox(
                           width: 10.0,
                         ),
                         ElevatedButton(
-                          onPressed: () {},
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (BuildContext context) {
+                                  return LoginScreen();
+                                },
+                              ),
+                            );
+                          },
                           child: Text('Se connecter'),
                         )
                       ],
@@ -94,7 +159,14 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
                       children: <Widget>[
                         Text('Vous n\'avez pas de compte ?'),
                         TextButton(
-                            onPressed: () {}, child: Text('Inscrivez-vous'))
+                            onPressed: () {
+                              Navigator.pushAndRemoveUntil(context,
+                                  PageRouteBuilder(
+                                      pageBuilder: (context, a, b) {
+                                return SignupScreen();
+                              }), (route) => false);
+                            },
+                            child: Text('Inscrivez-vous'))
                       ],
                     ),
                   ),
